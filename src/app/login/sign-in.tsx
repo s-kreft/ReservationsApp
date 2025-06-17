2c7a835bbcee498229504e48f6897683c0200478
s-kreft <sebastian.kreft.sg@gmail.com>
2025-06-17 20:48:49 +0200

    Add login logaut via GitHub button

--- src/app/login/sign-in.tsx
+++ src/app/login/sign-in.tsx
@@ -1,14 +1,24 @@
-import { signIn } from "@/auth";
+"use client";
+import React from "react";
+
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function SignIn() {
+  const session = useSession();
+
+  if (session.status === "authenticated") {
+    return (
+      <button className="btn" onClick={() => signOut({ callbackUrl: "/" })}>
+        {session.data.user?.name || session.data.user?.email}
+      </button>
+    );
+  } else if (session.status === "loading") {
+    return <button className="btn loading">Loading...</button>;
+  }
+
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("github");
-      }}
-    >
-      <button type="submit">Signin with GitHub</button>
-    </form>
+    <button className="btn" onClick={() => signIn("github")}>
+      Login
+    </button>
   );
 }
