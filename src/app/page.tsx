28f4e9a2c7dcacad09aa056a5cef322461b3b10e
s-kreft <sebastian.kreft.sg@gmail.com>
2025-06-18 19:25:09 +0200

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,27 @@ import Login from "./login/Login";
 import BookingForm from "./reservation/BookingForm";
 import MainDashboard from "./MainDashboard";
 import NavBar from "./components/NavBar";
+import { useEffect, useState, createContext } from "react";
+import credentials from "@/app/data/credentials.json";
+import { User } from "@/app/types";
+
+type UserContextType = {
+  users: User[];
+};
+export const UserContext = createContext<UserContextType>({ users: [] });
 
 export default function Home() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const loadedUsers = credentials as User[];
+
+    setUsers(loadedUsers);
+  }, []);
   return (
-    <>
+    <UserContext.Provider value={{ users }}>
       <NavBar></NavBar>
       <MainDashboard></MainDashboard>
-    </>
+    </UserContext.Provider>
   );
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,29 +1,18 @@
 import NextAuth, { type DefaultSession } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { DataSourceContext } from "./data/DataSource";
 
 declare module "next-auth" {
-  /**
-   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
   interface Session {
     user: {
-      /** The user's role. */
       role: string;
-      /**
-       * By default, TypeScript merges new interface properties and overwrites existing ones.
-       * In this case, the default session user properties will be overwritten,
-       * with the new ones defined above. To keep the default session user properties,
-       * you need to add them back into the newly declared interface.
-       */
     } & DefaultSession["user"];
   }
 }
 
-// The `JWT` interface can be found in the `next-auth/jwt` submodule
 import { JWT } from "next-auth/jwt";
 
 declare module "next-auth/jwt" {
-  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
   interface JWT {
     role: string;
   }
@@ -46,7 +35,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     jwt({ token, user }) {
       if (user) {
         token.id = user.id;
-        token.role = "user";
+        let cachedUser = DataSourceContext.users.find(
+          (u) => u.name === user.name
+        );
+        if (cachedUser) {
+          token.role = "admin";
+        } else {
+          token.role = "user";
+        }
       }
       return token;
     },
